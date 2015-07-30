d95ea22666cf51512f3a50a786a6b149cca9914b
manuel_ruelke <manuel.ruelke@sitewards.com>
2015-07-30 08:19:08 +0200

    added build chain to gulp file (compressing, adding license)

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp'),
     Server = require('karma').Server,
-    jshint = require('gulp-jshint')
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat-util'),
+    fs = require('fs'),
+    clean = require('gulp-clean'),
+    rename = require('gulp-rename')
 ;
 
 /**
@@ -31,4 +36,36 @@ gulp.task('lint', function() {
         .pipe(jshint.reporter('default'));
 });
 
-gulp.task('default', ['lint', 'test']);
+/**
+ * compress js
+ */
+gulp.task('compress', ['clean'], function() {
+    return gulp.src('js/*.js')
+        .pipe(uglify())
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+
+/**
+ * Prepend license text to the js files
+ */
+gulp.task('license', ['compress'], function () {
+    var license = fs.readFileSync('LICENSE_SHORT');
+
+    return gulp.src('dist/*.js')
+        .pipe(concat.header(license))
+        .pipe(gulp.dest('dist'));
+});
+
+/**
+ * Clean up destination folder
+ */
+gulp.task('clean', function () {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('default', ['lint', 'test', 'clean', 'compress', 'license']);
